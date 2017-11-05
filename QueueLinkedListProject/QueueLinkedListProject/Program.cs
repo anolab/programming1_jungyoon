6f43df2d397118b8717e9e2dca4913736e429929
lifesailor <sailyourlife91@gmail.com>
2017-11-05 21:18:52 +0900

    5-2. QueueLinkedListProject 생성 및 구현

--- QueueLinkedListProject/QueueLinkedListProject/Program.cs
+++ QueueLinkedListProject/QueueLinkedListProject/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace StackArrayProject
+namespace QueueLinkedListProject
 {
     class Program
     {
@@ -12,15 +12,15 @@ namespace StackArrayProject
         {
             int choice, x;
 
-            StackA st = new StackA(8);
+            QueueL ql = new QueueL();
 
             while (true)
             {
-                Console.WriteLine("1. Push an element on the stack");
-                Console.WriteLine("2. Pop an element from the stack");
+                Console.WriteLine("1. Push an element on the queue");
+                Console.WriteLine("2. Pop an element from the queue");
                 Console.WriteLine("3. Display the top elements");
-                Console.WriteLine("4. Display all stack members");
-                Console.WriteLine("5. Display size of the stack");
+                Console.WriteLine("4. Display all queue members");
+                Console.WriteLine("5. Display size of the queue");
                 Console.WriteLine("6. Quit");
                 Console.Write("Enter your choice: ");
                 choice = Convert.ToInt32(Console.ReadLine());
@@ -33,24 +33,24 @@ namespace StackArrayProject
                     case 1:
                         Console.Write("Enther the element to be pushed: ");
                         x = Convert.ToInt32(Console.ReadLine());
-                        st.Push(x);
+                        ql.Insert(x);
                         break;
 
                     case 2:
-                        x = st.Pop();
+                        x = ql.Delete();
                         Console.WriteLine("Popped element is " + x);
                         break;
 
                     case 3:
-                        Console.WriteLine("Element at the top is " + st.Peek());
+                        Console.WriteLine("Element at the front is " + ql.Peek());
                         break;
 
                     case 4:
-                        st.Display();
+                        ql.Display();
                         break;
 
                     case 5:
-                        Console.WriteLine("Size of stack " + st.Size());
+                        Console.WriteLine("Size of stack " + ql.Size());
                         break;
 
                     default:
---
+++ QueueLinkedListProject/QueueLinkedListProject/QueueL.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueLinkedListProject
+{
+    class QueueL
+    {
+        private Node front;
+        private Node rear;
+
+        public QueueL()
+        {
+            front = null;
+            rear = null;
+        }
+
+        public bool isEmpty()
+        {
+            return (front == null);
+        }
+        public int Size()
+        {
+            int s = 0;
+            Node p = front;
+            while(p != null)
+            {
+                s++;
+                p = p.link;
+            }
+            return s;
+        }
+
+        public void Insert(int x)
+        {
+            Node temp;
+            temp = new Node(x);
+
+            if (front == null)
+                front = temp;
+            else
+                rear.link = temp;
+            rear = temp; 
+        }
+
+        public int Delete()
+        {
+            int x;
+            if (isEmpty())
+                throw new System.InvalidOperationException("Queue Underflow");
+            x = front.info;
+            front = front.link;
+            return x;
+        }
+
+        public int Peek()
+        {
+            if (isEmpty())
+                throw new System.InvalidOperationException("Queue Underflow");
+            return front.info;
+        }
+
+        public void Display()
+        {
+            Node p = front;
+            if (isEmpty())
+            {
+                Console.WriteLine("Queue is Empty");
+                return;
+            }
+
+            Console.WriteLine("Queue is ");
+            while(p!= null)
+            {
+                Console.Write(p.info + " ");
+                p = p.link;
+            }
+            Console.WriteLine();
+        }
+    }
+}
